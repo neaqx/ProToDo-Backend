2d16e0b42c987a55e7a27851a11f3c8e3cfcd713
Nevin Aquinas <87398867+neaqx@users.noreply.github.com>
2024-11-25 11:52:15 +0000

    Add comment model to enable communication between user

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -33,4 +33,13 @@ class Task(models.Model):
     def __str__(self):
         return self.title
 
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.task.title}'
+
 
